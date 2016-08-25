dfe916f51777c3d7786b947250522aeb0e6109c9
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2016-08-25 11:59:47 -0400

    New project

--- js/shadergif.js
+++ js/shadergif.js
@@ -38,9 +38,6 @@ var gif_canvas = qsa(".gif-canvas")[0];
 gif_canvas.width = 500;
 gif_canvas.height = 500;
 
-var res_ctx = canvas.getContext("webgl");
-var gif_ctx = gif_canvas.getContext("webgl");
-
 var fragment_error_pre = qsa(".fragment-error-pre")[0];
 var vertex_error_pre = qsa(".vertex-error-pre")[0];
 
@@ -81,29 +78,6 @@ function enable_mouse(can){
     });
 }
 
-init_ctx(res_ctx);
-init_ctx(gif_ctx);
-
-function init_ctx(ctx){
-    ctx.clearColor(0.0, 0.0, 0.0, 1.0);
-    ctx.enable(ctx.DEPTH_TEST);
-    ctx.depthFunc(ctx.LEQUAL);
-    ctx.clear(ctx.COLOR_BUFFER_BIT | ctx.DEPTH_BUFFER_BIT);
-
-    // Triangle strip for whole screen square
-    var vertices = [
-            -1,-1,0,
-            -1,1,0,
-        1,-1,0,
-        1,1,0,
-    ];
-    
-    var tri = ctx.createBuffer();
-    ctx.bindBuffer(ctx.ARRAY_BUFFER,tri);
-    ctx.bufferData(ctx.ARRAY_BUFFER, new Float32Array(vertices), ctx.STATIC_DRAW);
-}
-
-var vertex_code = load_script("vertex-shader");
 var fragment_code = qsa("textarea[name='fragment']")[0];
 
 // Enable codemirror
@@ -129,136 +103,10 @@ if(filename != ""){
     }
 }
 
-f_editor.on("change", update_shader);
+var can = three_canvas({canvas: canvas});
+var gif_can = three_canvas({canvas: gif_canvas});
 
-update_shader();
-
-function update_shader(){
-    init_program(res_ctx);
-    init_program(gif_ctx);
-}
-
-function init_program(ctx){
-    ctx.program = ctx.createProgram();
-        
-    var vertex_shader =
-        add_shader(ctx.VERTEX_SHADER, vertex_code);
-    
-    var fragment_shader =
-        add_shader(ctx.FRAGMENT_SHADER, f_editor.getValue());
-    
-    function add_shader(type,content){
-        var shader = ctx.createShader(type);
-        ctx.shaderSource(shader,content);
-        ctx.compileShader(shader);
-
-        // Find out right error pre
-        var type_pre = type == ctx.VERTEX_SHADER ?
-            vertex_error_pre:
-            fragment_error_pre;
-        
-        if(!ctx.getShaderParameter(shader, ctx.COMPILE_STATUS)){
-            var err = ctx.getShaderInfoLog(shader);
-
-            // Find shader type
-            var type_str = type == ctx.VERTEX_SHADER ?
-                "vertex":
-                "fragment";
-            
-            type_pre.textContent =
-                "Error in " + type_str + " shader.\n" +
-                err;
-        } else {
-            type_pre.textContent = "";
-        }
-        
-        ctx.attachShader(ctx.program, shader);
-        return shader;
-    }
-    
-    ctx.linkProgram(ctx.program);
-    
-    if(!ctx.getProgramParameter(ctx.program, ctx.LINK_STATUS)){
-        console.log(ctx.getProgramInfoLog(ctx.program));
-    }
-    
-    ctx.useProgram(ctx.program);
-
-    var positionAttribute = ctx.getAttribLocation(ctx.program, "position");
-    
-    ctx.enableVertexAttribArray(positionAttribute);
-    ctx.vertexAttribPointer(positionAttribute, 3, ctx.FLOAT, false, 0, 0);
-    
-}
-
-function draw_ctx(can, ctx, time){
-    // Set time attribute
-    var tot_time = anim_len * anim_delay;
-
-    var time = time ||
-        parseFloat(
-            ((new Date()).getTime() % tot_time)
-                /
-                tot_time
-        );
-    
-    var timeAttribute = ctx.getUniformLocation(ctx.program, "time");
-    ctx.uniform1f(timeAttribute, time);
-    
-    // Screen ratio
-    var ratio = can.width / can.height;
-
-    var ratioAttribute = ctx.getUniformLocation(ctx.program, "ratio");
-    ctx.uniform1f(ratioAttribute, ratio);
-
-    // Mouse
-    var x = mouse[0] / can.width * ratio;
-    var y = - mouse[1] / can.height;
-    var mouseAttribute = ctx.getUniformLocation(ctx.program, "mouse");
-    ctx.uniform2fv(mouseAttribute, [x, y]);
-
-    // Smooth mouse
-    if(can.hover == true){
-        smooth_mouse[0] = 0.9 * smooth_mouse[0] + 0.1 * x;
-        smooth_mouse[1] = 0.9 * smooth_mouse[1] + 0.1 * y;
-    }
-
-    var smAttribute = ctx.getUniformLocation(
-        ctx.program, "smooth_mouse"
-    );
-    
-    ctx.uniform2fv(smAttribute, smooth_mouse);
-    
-    ctx.drawArrays(ctx.TRIANGLE_STRIP, 0, 4);
-
-    ctx.viewport(0, 0, can.width, can.height);
-
-}
-
-var rendering_gif = false;
-
-function draw(){
-    draw_ctx(canvas, res_ctx);
-
-    window.requestAnimationFrame(draw);
-}
-
-window.requestAnimationFrame(draw);
-
-setInterval(
-    function(){
-        frame++;
-        frame = frame % (anim_len);
-        
-        window.requestAnimationFrame(function(){
-            // When rendering gif, draw is done elsewhere
-            if(!rendering_gif){
-                draw_ctx(gif_canvas, gif_ctx, (frame + 1)/(anim_len));
-            }
-        });
-    }
-    , anim_delay
-);
+//f_editor.on("change", );
 
 var gif_button = qsa("button[name='make-gif']")[0];
 
@@ -271,15 +119,14 @@ function make_gif(){
     to_export.delay = anim_delay;
     to_export.data = [];
     
-    rendering_gif = true;
+    gif_can.stop();
     
     for(var i = 0; i < anim_len; i++){
-        draw_ctx(gif_canvas, gif_ctx, (i + 1)/anim_len);
-        
+        gif_can.render();
         to_export.data.push(gif_canvas.toDataURL());
     }
 
-    rendering_gif = false;
+    gif_can.start();
     
     export_gif(to_export);
 }
